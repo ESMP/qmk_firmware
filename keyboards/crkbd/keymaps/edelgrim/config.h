fffe0f75aaae30e639438230be4e90a1f0ae939d
Eduardo Sánchez <622343+ESMP@users.noreply.github.com>
2025-02-18 10:45:35 -0600

    Updated config file

--- keyboards/crkbd/keymaps/edelgrim/config.h
+++ keyboards/crkbd/keymaps/edelgrim/config.h
@@ -27,6 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
+// #define SPLIT_HAND_PIN B7
+// #define	SPLIT_HAND_PIN_LOW_IS_LEFT
 
 //#define QUICK_TAP_TERM 0
 //#define TAPPING_TERM 100
